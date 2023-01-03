573678c634f9c04d7e2e4497254606adb6c3df40
VVViktorVVV <86729213+VVViktorVVV@users.noreply.github.com>
2023-01-04 00:51:14 +0200

    тестовий масив 2

--- src/componens/functions/Filter.js
+++ src/componens/functions/Filter.js
@@ -2,14 +2,13 @@
 import arrayOrder from "../arrays/arrayOrder";
 // import arrayGZ from "./arrays/arrayGZ";
 import arrayLost from "../arrays/arrayLost";
+// import creatArrayWithPeriodDays from "./ArrayWithPeriodDays";
 
 const finisharray = [];
 
 const creatFinishArray = () => {
 
     finisharray.length = 0;    
-    console.log(arrayLost);
-
     arrayOrder.forEach((el) => {
         if (!arrayLost.includes(el.id)) {
             finisharray.push(el)
